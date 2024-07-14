38979252c6415f99cd53fd6477809f0b5bb4396a
Anushka Mahesh <anushka@akoomtech.co.uk>
2024-07-14 23:09:30 +0100

    resolve locatio issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://anushkamds.github.io",
-  base: "https://github.com/anushkamds/anushkamds.github.io",
   integrations: [
     tailwind(),
     sitemap({
